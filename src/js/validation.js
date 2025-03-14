4301c39cab4f3384d5d8ac046481dfcd34273952
Emelie Barthelsson <emeliebarthelsson@gmail.com>
2025-03-14 19:43:46 +0100

    fix bugs in validation, edit and assign, add check for number of students in courses

--- src/js/validation.js
+++ src/js/validation.js
@@ -2,26 +2,19 @@ class Validation {
     static validateForm(target, validationMessage) {
         validationMessage.style.display = "none";
         const fieldsToValidate = [];
-        if (target === "students") {
+        if (target === "students" || target === "instructor") {
             fieldsToValidate.push(
                 { name: "firstName", message: "Please enter your first name"},
                 { name: "lastName", message: "Please enter your last name"},
                 { name: "email", message: "Please enter your email"},
                 { name: "phone", message: "Please enter your phone number"}
             )
-        } else if (target === "instructor") {
-            fieldsToValidate.push(
-                { name: "firstName", message: "Please enter your first name"},
-                { name: "lastName", message: "Please enter your last name"},
-                { name: "email", message: "Please enter your email"},
-                { name: "phone", message: "Please enter your phone number"}
-            ) 
         } else if (target === "course") {
             fieldsToValidate.push(
                 { name: "courseName", message: "Please enter course name"},
                 { name: "courseCode", message: "Please enter course code"}
-            )
-        }
+            );
+        };
 
         for (let field of fieldsToValidate) {
             const inputField = document.querySelector(`[id = ${field.name}]`);
@@ -34,6 +27,25 @@ class Validation {
                 validationMessage.textContent = field.message;
                 inputField.classList.add("invalid-input");
                 return false;
+            }
+
+            if (field.name === "phone") {
+                const validPhoneNumber = /^\d{8}$/;
+                if (!validPhoneNumber.test(inputField.value)) {
+                    validationMessage.style.display = "block";
+                    validationMessage.textContent = "The phone number must consist of 8 digits";
+                    inputField.classList.add("invalid-input");
+                    return false;
+                }
+            }
+
+            if (field.name === "email") {
+                if (!inputField.value.includes("@")) {
+                    validationMessage.style.display = "block";
+                    validationMessage.textContent = "Email must contain '@'";
+                    inputField.classList.add("invalid-input");
+                    return false;
+                }
             }
         }
         return true;
